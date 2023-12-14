bdee2549956bfd533c28668c4ff9686091363140
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-12-14 06:56:46 +0100

    migrating to cmp

--- lua/mycelium/init.lua
+++ lua/mycelium/init.lua
@@ -2,6 +2,7 @@ local mycelium = {}
 
 local Job = require('plenary.job')
 local json = vim.json
+local cmp = require('cmp')
 
 -- Function to get the current prompt from the editor
 function mycelium.getPrompt()
@@ -11,7 +12,7 @@ end
 -- Function to make a cURL request to the localhost server
 function mycelium.makeCurlRequest(prompt, callback)
     local data = json.encode({ model = "llama2", prompt = prompt, stream = false })
-    print("Sending request with data: " .. data)  -- Print the sent request
+    print("Sending request with data: " .. data)
     Job:new({
         command = 'curl',
         args = {'-X', 'POST', 'http://localhost:11434/api/generate', '-d', data},
@@ -21,28 +22,30 @@ function mycelium.makeCurlRequest(prompt, callback)
     }):start()
 end
 
--- Function to display the response as an overlay
-function mycelium.displayOverlay(response)
-    vim.schedule(function()
-        local bufnr = vim.api.nvim_create_buf(false, true)
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, response)
-        local win_id = vim.api.nvim_open_win(bufnr, false, {
-            relative = 'cursor',
-            width = 50,
-            height = 10,
-            col = 1,
-            row = 1,
-            style = 'minimal',
-            border = 'rounded'
-        })
-        vim.api.nvim_win_set_option(win_id, 'winblend', 20) -- Set the window as semi-transparent
-    end)
+-- Completion source for nvim-cmp
+local source = {}
+
+source.new = function()
+  return setmetatable({}, { __index = source })
 end
+
+source.complete = function(self, request, callback)
+  local prompt = mycelium.getPrompt()
+  mycelium.makeCurlRequest(prompt, function(result)
+    local items = vim.tbl_map(function(item)
+      return { label = item }
+    end, result)
+    callback({ items = items, isIncomplete = true })
+  end)
+end
+
+-- Register the completion source
+cmp.register_source('mycelium', source.new())
 
 -- Main function to generate text
 function mycelium.generateText()
     local prompt = mycelium.getPrompt()
-    mycelium.makeCurlRequest(prompt, mycelium.displayOverlay)
+    mycelium.makeCurlRequest(prompt, function(result) end)
 end
 
 -- Command to trigger the text generation
