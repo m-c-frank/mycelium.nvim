f1985e7b2d5d9183989f7e386e1fd9027546ff3d
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-12-24 09:12:41 +0100

    trying to fix stuff

--- lua/mycelium/init.lua
+++ lua/mycelium/init.lua
@@ -1,10 +1,8 @@
 local mycelium = {}
 
--- Import required modules
 local Job = require('plenary.job')
 local json = vim.json
 
--- Configuration
 local config = {
     max_prompt_length = 512,
     generate_url = 'http://localhost:11434/api/generate',
@@ -13,18 +11,18 @@ local config = {
     stream = true,
 }
 
--- Function to get the current prompt from the editor
 function mycelium.getPrompt()
     local full_prompt = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
     return string.sub(full_prompt, 1, config.max_prompt_length)
 end
 
--- Function to make a streaming cURL request to the server
 function mycelium.makeCurlRequest(url, requestData, callback)
+    print("Making Curl Request") -- Debug print
     Job:new({
         command = 'curl',
         args = {'-X', 'POST', url, '-d', json.encode(requestData)},
         on_stdout = function(_, response)
+            print("Response received: " .. response) -- Debug print
             if response then
                 local json_response = json.decode(response)
                 if json_response and json_response.response then
@@ -37,17 +35,14 @@ function mycelium.makeCurlRequest(url, requestData, callback)
     }):start()
 end
 
--- Namespace ID for extmarks
 local ns_id = vim.api.nvim_create_namespace('mycelium_namespace')
 
--- Function to create or update an extmark with virtual text
 function mycelium.createOrUpdateExtMark(buffer, line, text)
     local virt_text = {{text, 'Comment'}}
     local opts = { virt_text = virt_text, virt_text_pos = "eol" }
     vim.api.nvim_buf_set_extmark(buffer, ns_id, line, 0, opts)
 end
 
--- Function to display the response as virtual text
 function mycelium.displayResponse(response)
     if response then
         vim.schedule(function()
@@ -57,21 +52,19 @@ function mycelium.displayResponse(response)
     end
 end
 
--- Function to generate text on space press
 function mycelium.spaceTrigger()
     local last_char = vim.api.nvim_get_vvar("char")
+    print("Space Triggered") -- Debug print
     if last_char == ' ' then
         mycelium.generateText()
     end
 end
 
--- Main function to generate text
 function mycelium.generateText()
     local prompt = mycelium.getPrompt()
     mycelium.makeCurlRequest(config.generate_url, { model = config.model, prompt = prompt, stream = config.stream }, mycelium.displayResponse)
 end
 
--- Function to stop the ongoing generation
 function mycelium.stopOllamaGeneration()
     Job:new({
         command = 'curl',
@@ -79,12 +72,10 @@ function mycelium.stopOllamaGeneration()
     }):start()
 end
 
--- Autocommand to trigger text generation on pressing space
 vim.api.nvim_create_autocmd('InsertCharPre', {
     callback = mycelium.spaceTrigger
 })
 
--- Commands to trigger the text generation and stop functions
 vim.api.nvim_create_user_command('Gen', mycelium.generateText, {})
 vim.api.nvim_create_user_command('StopGen', mycelium.stopOllamaGeneration, {})
 
