411644fb3736e594d419b6398a812cd13c1ed531
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-12-24 08:56:18 +0100

    starting to implement streaming and preparing stop

--- lua/mycelium/init.lua
+++ lua/mycelium/init.lua
@@ -1,30 +1,39 @@
 local mycelium = {}
 
+-- Import required modules
 local Job = require('plenary.job')
 local json = vim.json
 
+-- Configuration
+local config = {
+    max_prompt_length = 512,
+    generate_url = 'http://localhost:11434/api/generate',
+    stop_url = 'http://localhost:11434/api/stop',
+    model = "mistral",
+    stream = true,
+}
+
 -- Function to get the current prompt from the editor
 function mycelium.getPrompt()
-    return table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
+    local full_prompt = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
+    return string.sub(full_prompt, 1, config.max_prompt_length)
 end
 
--- Function to make a cURL request to the localhost server
-function mycelium.makeCurlRequest(prompt, callback)
-    local data = json.encode({ model = "llama2", prompt = prompt, stream = false })
-    print("Sending request with data: " .. data)
+-- Function to make a streaming cURL request to the server
+function mycelium.makeCurlRequest(url, requestData, callback)
     Job:new({
         command = 'curl',
-        args = {'-X', 'POST', 'http://localhost:11434/api/generate', '-d', data},
-        on_exit = function(j)
-            local result_table = j:result()
-            local result_str = table.concat(result_table, "\n") -- Concatenate table into string
-            local result = json.decode(result_str)
-            if result and result.response then
-                callback(result.response)
-            else
-                callback(result_str) -- Return the entire text if 'response' field does not exist
+        args = {'-X', 'POST', url, '-d', json.encode(requestData)},
+        on_stdout = function(_, response)
+            if response then
+                local json_response = json.decode(response)
+                if json_response and json_response.response then
+                    callback(json_response.response)
+                end
             end
-        end
+        end,
+        stdout_buffered = false,
+        stderr_buffered = false,
     }):start()
 end
 
@@ -32,9 +41,9 @@ end
 local ns_id = vim.api.nvim_create_namespace('mycelium_namespace')
 
 -- Function to create or update an extmark with virtual text
-function mycelium.createOrUpdateExtMark(buffer, line, text, highlight_group, virt_text_pos)
-    local virt_text = {{text, highlight_group}}
-    local opts = { virt_text = virt_text, virt_text_pos = virt_text_pos or "eol" }
+function mycelium.createOrUpdateExtMark(buffer, line, text)
+    local virt_text = {{text, 'Comment'}}
+    local opts = { virt_text = virt_text, virt_text_pos = "eol" }
     vim.api.nvim_buf_set_extmark(buffer, ns_id, line, 0, opts)
 end
 
@@ -42,20 +51,42 @@ end
 function mycelium.displayResponse(response)
     if response then
         vim.schedule(function()
-            local line = vim.api.nvim_win_get_cursor(0)[1] - 1 -- Current line (0-based index)
-            mycelium.createOrUpdateExtMark(0, line, response, 'Comment', 'eol')
+            local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+            mycelium.createOrUpdateExtMark(0, line, response)
         end)
+    end
+end
+
+-- Function to generate text on space press
+function mycelium.spaceTrigger()
+    local last_char = vim.api.nvim_get_vvar("char")
+    if last_char == ' ' then
+        mycelium.generateText()
     end
 end
 
 -- Main function to generate text
 function mycelium.generateText()
     local prompt = mycelium.getPrompt()
-    mycelium.makeCurlRequest(prompt, mycelium.displayResponse)
+    mycelium.makeCurlRequest(config.generate_url, { model = config.model, prompt = prompt, stream = config.stream }, mycelium.displayResponse)
 end
 
--- Command to trigger the text generation
+-- Function to stop the ongoing generation
+function mycelium.stopOllamaGeneration()
+    Job:new({
+        command = 'curl',
+        args = {'-X', 'POST', config.stop_url}
+    }):start()
+end
+
+-- Autocommand to trigger text generation on pressing space
+vim.api.nvim_create_autocmd('InsertCharPre', {
+    callback = mycelium.spaceTrigger
+})
+
+-- Commands to trigger the text generation and stop functions
 vim.api.nvim_create_user_command('Gen', mycelium.generateText, {})
+vim.api.nvim_create_user_command('StopGen', mycelium.stopOllamaGeneration, {})
 
 return mycelium
 
