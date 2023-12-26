909d2e12f73626ea985d18eb7df3c3d5e9211317
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-12-26 10:08:35 +0100

    added debug

--- lua/mycelium/init.lua
+++ lua/mycelium/init.lua
@@ -26,7 +26,9 @@ function mycelium.makeCurlRequest(url, requestData, callback)
         command = 'curl',
         args = { url, '-d', jsonData, '-H', 'Content-Type: application/json' },
         on_exit = function(j)
-            local response = json.decode(table.concat(j:result(), ""))
+            local result = j.result()
+            print(result)
+            local response = json.decode(table.concat(result, ""))
             if callback then
                 callback(response)
             end
